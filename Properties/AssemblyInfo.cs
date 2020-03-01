b9955bdbae2fa3a2b5af84001416d4ad2c88a942
Pierce Corcoran <pierce@plasticcow.com>
2020-03-01 14:15:16 -0800

    Simplify project structure & use harmony mod

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ThermalUnits")]
+[assembly: AssemblyVersion("1.0.0")]
---
+++ Source/Mod.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+using HarmonyLib;
+
+namespace ThermalUnits
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            new Harmony("MySteamUsername.rimworld.ThermalUnits.main").PatchAll();
+        }
+    }
+}
